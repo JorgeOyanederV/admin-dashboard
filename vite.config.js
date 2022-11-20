a65f1ba3d250564bc3c9d5903c4c1ffce8fbc119
Jorge Oyaneder <joyaneder1996@gmail.com>
2022-11-20 18:44:04 -0300

    fixing dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/admin-dashboard/'
 })
+
+// https://jorgeoyanederv.github.io/admin-dashboard/
