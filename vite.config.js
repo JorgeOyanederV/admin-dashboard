be8c658a7432349f3ed997b6c44b714272d225cd
Jorge Oyaneder <joyaneder1996@gmail.com>
2022-11-22 14:16:26 -0300

    Fix rute?

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin-dashboard/'
+  base: '/admin-dashboard/#'
 })
 
 // https://jorgeoyanederv.github.io/admin-dashboard/
